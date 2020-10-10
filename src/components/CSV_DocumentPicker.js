dd0adaceddd7e3da610807df0a92b8bbd3250a07
K <gemigpost@hotmail.com>
2020-10-10 19:04:58 +0200

    CSV_DocumentPicker refactor/cleanup

--- src/components/CSV_DocumentPicker.js
+++ src/components/CSV_DocumentPicker.js
@@ -1,43 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
-//import { DocumentPicker, ImagePicker } from 'expo';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 import { withNavigation } from 'react-navigation';
 import PresetContext from '../context/preset/presetContext';
 import { theme } from '../constants';
-import AlertContext from '../context/alert/alertContext';
 
 const CSV_DocumentPicker = () => {
-  // context alert
-  const alertContext = React.useContext(AlertContext);
-  const { setAlert } = alertContext;
-  //context
-
-  const { uploadCSV, csvpresets, filterPresets, contacterror, clearContactError } = React.useContext(PresetContext);
+  //context presets
+  const { uploadCSV } = React.useContext(PresetContext);
 
   // state
   const [file, setFile] = React.useState(null);
 
+  // logic
   const _pickDocument = async () => {
     let result = await DocumentPicker.getDocumentAsync({});
-    //alert(result.uri);
-    //console.log(result);
     result && result.type === 'success' && setFile(result);
-    // console.log(presetContext.month);
-    //console.log('pickdocument ran');
-    // console.log(result);
   };
+
+  // useeffect
   React.useEffect(() => {
     if (file) {
-      // console.log(file);
-      // console.log('useEffect file ran');
       const formData = new FormData();
       formData.append('file', file, file.name);
       uploadCSV(formData);
     }
   }, [file]);
 
-  //csvpresets && console.log(csvpresets.map((preset) => preset.name));
   return (
     <TouchableOpacity onPress={_pickDocument} style={styles.container}>
       <Text>Upload CSV-file</Text>
@@ -47,7 +36,6 @@ const CSV_DocumentPicker = () => {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
     paddingVertical: 15,
     paddingHorizontal: 0,
     marginHorizontal: 0,
